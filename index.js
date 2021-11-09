ee60bf0089b79ba1ac54e7916de8897bef7a9eb0
douglassmoss <douglassmoss@icloud.com>
2021-11-08 17:06:13 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(num) {
+  return num + num
+}
+function subtract(num) {
+  return num - num
+}
+function multiply(num) {
+  return num * num
+}
+function divide(num) {
+  return num / num
+}
+function add(a, b){
+  return a + b
+}
+function subtract(a, b){
+  return a - b
+}
+function multiply(a, b){
+  return a * b
+}
+function divide(a, b){
+  return a / b
+}
+function increment(n){
+  return (n+1)
+}
+function decrement(n){
+  return (n-1)
+}
+function makeInt(n){
+  return parseInt(n, 10)
+}
+function preserveDecimal(string){
+  return parseFloat(string)
+}
